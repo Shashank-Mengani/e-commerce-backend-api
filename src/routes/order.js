6cb8d045b0afb50c8e5b96a9e9f69b4f6c22043f
Shashank <135490372+Shashank-Mengani@users.noreply.github.com>
2026-04-28 12:34:47 +0530

    Remove unused order routes in order.js

--- src/routes/order.js
+++ src/routes/order.js
@@ -16,22 +16,16 @@ import {
 
 const router = express.Router();
 
-// CREATE
 router.post("/", verifyToken, createOrder);
 
-// UPDATE (ADMIN)
 router.put("/:id", verifyTokenAndAdmin, updateOrder);
 
-// DELETE (ADMIN)
 router.delete("/:id", verifyTokenAndAdmin, deleteOrder);
 
-// GET USER ORDERS
 router.get("/find/:userId", verifyTokenAndAuthorization, getUserOrders);
 
-// GET ALL (ADMIN)
 router.get("/", verifyTokenAndAdmin, getAllOrders);
 
-// GET MONTHLY INCOME
 router.get("/income", verifyTokenAndAdmin, getMonthlyIncome);
 
 export default router;

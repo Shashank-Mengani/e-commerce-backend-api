e912941c3d113dd8023d504e754b9d0e086ed227
Shashank <135490372+Shashank-Mengani@users.noreply.github.com>
2026-04-28 12:33:12 +0530

    Remove commented-out code from user routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -14,19 +14,14 @@ import {
 
 const router = express.Router();
 
-// UPDATE
 router.put("/:id", verifyTokenAndAuthorization, updateUser);
 
-// DELETE
 router.delete("/:id", verifyTokenAndAuthorization, deleteUser);
 
-// GET USER
 router.get("/find/:id", verifyTokenAndAdmin, getUser);
 
-// GET ALL USERS
 router.get("/", verifyTokenAndAdmin, getAllUsers);
 
-// GET STATS
 router.get("/stats", verifyTokenAndAdmin, getUserStats);
 
 export default router;

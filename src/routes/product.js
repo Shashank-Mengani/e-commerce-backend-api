d65bca58678d98b417e42378169d685c86b91b7a
Shashank <135490372+Shashank-Mengani@users.noreply.github.com>
2026-04-28 12:35:24 +0530

    Remove product route handlers

--- src/routes/product.js
+++ src/routes/product.js
@@ -13,19 +13,14 @@ import {
 
 const router = express.Router();
 
-// CREATE
 router.post("/", verifyTokenAndAdmin, createProduct);
 
-// UPDATE
 router.put("/:id", verifyTokenAndAdmin, updateProduct);
 
-// DELETE
 router.delete("/:id", verifyTokenAndAdmin, deleteProduct);
 
-// GET ONE
 router.get("/find/:id", getProduct);
 
-// GET ALL
 router.get("/", getProducts);
 
 export default router;
